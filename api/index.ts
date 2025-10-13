890ac8b78646c778a31b15db417142fd541bf445
Leandro Brito <klonoa51@gmail.com>
2025-10-13 11:42:26 -0300

    ajustando scripts de deploy 2

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-// api/index.ts
-import { getServer } from '../dist/vercel'; // usa o build compilado
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  const server = await getServer();
-  // como estamos com Express, apenas delegamos:
-  server(req, res);
-}
